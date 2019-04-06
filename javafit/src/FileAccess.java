d96a0be0cf55d7fbeabab5e2be5c4fc2aff4d81c
Chris Springstead <chrisspringstead@Chriss-MacBook-Pro-2.local>
2019-04-06 10:31:06 -0500

    fixed formatting

--- javafit/src/FileAccess.java
+++ javafit/src/FileAccess.java
@@ -1,10 +1,8 @@
 // Write code to load the workouts from the provided workouts.csv file. The function should return a Workouts object.
 
 public class FileAccess {
-  
-  public static Workouts loadWorkouts() {
-    // What is a try/catch block and why do we need one?
-    // What is an exception?
-  }
-
+	public static Workouts loadWorkouts() {
+    	// What is a try/catch block and why do we need one?
+    	// What is an exception?
+  	}
 }
